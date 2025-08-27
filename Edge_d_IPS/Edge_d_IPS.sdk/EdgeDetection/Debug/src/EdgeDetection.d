e9354db7ce724333a45338c65b53a29eeb6b5eb0
vinay <vinayy232@gmail.com>
2025-08-28 01:27:45 +0530

    update code

---
+++ Edge_d_IPS/Edge_d_IPS.sdk/EdgeDetection/Debug/src/EdgeDetection.d
@@ -0,0 +1,82 @@
+src/EdgeDetection.o src/EdgeDetection.o: ../src/EdgeDetection.c \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xaxidma.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xaxidma_bd.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xaxidma_hw.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xdebug.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/sleep.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xscugic.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../src/imageData.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xuartps.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xuartps_hw.h \
+ ../../EdgeDetection_bsp/ps7_cortexa9_0/include/xplatform_info.h
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xaxidma.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xaxidma_bd.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xaxidma_hw.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xdebug.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/sleep.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xscugic.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../src/imageData.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xuartps.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xuartps_hw.h:
+
+../../EdgeDetection_bsp/ps7_cortexa9_0/include/xplatform_info.h:
---
+++ Edge_d_IPS/Edge_d_IPS.sdk/imageIpTest/Debug/src/imageIpTest.d
@@ -0,0 +1,82 @@
+src/imageIpTest.o src/imageIpTest.o: ../src/imageIpTest.c \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xaxidma.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xaxidma_bd.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xaxidma_hw.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xdebug.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/sleep.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xscugic.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../src/imageData.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xuartps.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xuartps_hw.h \
+ ../../imageIpTest_bsp/ps7_cortexa9_0/include/xplatform_info.h
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xaxidma.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xaxidma_bd.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xaxidma_hw.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xdebug.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/sleep.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xscugic.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../src/imageData.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xuartps.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xuartps_hw.h:
+
+../../imageIpTest_bsp/ps7_cortexa9_0/include/xplatform_info.h:
